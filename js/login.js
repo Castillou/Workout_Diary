013ed067f5caf44dd60ba3b87fd952b083b6f0f8
Castillou <joajoa70584@gmail.com>
2024-09-01 17:39:14 +0900

    CSS & JS Guide line update

--- js/login.js
+++ js/login.js
@@ -1,1 +1,5 @@
 // Swan
+/* login display 구현 */
+// STEP 1 : login_form에 "submit" event 발생 => login_input 값 => welcome 텍스트로 저장
+// STEP 2 : welcome, workout_add => hidden class 제거
+// STEP 3 : login_form => hidden class 제거, workouts => height: 50vh
