a4293dabaf9eade55c67b33a3db485cba5ef3273
Castillou <joajoa70584@gmail.com>
2024-09-02 15:34:25 +0900

    Fifth commit

--- js/login.js
+++ js/login.js
@@ -3,3 +3,69 @@
 // STEP 1 : login_form에 "submit" event 발생 => login_input 값 => welcome 텍스트로 저장
 // STEP 2 : welcome, workout_add => hidden class 제거
 // STEP 3 : login_form => hidden class 제거, workouts => height: 50vh
+
+const loginForm = document.querySelector('.login_form');
+const loginInput = document.querySelector('.login_form input');
+const welcome = document.querySelector('.welcome');
+const workouts = document.querySelector('.workouts');
+const workoutAdd = document.querySelector('.workout_add');
+const workoutForm = document.querySelector('.workout_form');
+const imgBox = document.querySelector('.img_box');
+const dotBox = document.querySelector('.dot_box');
+
+const logoutBtn = document.querySelector('.logout_btn');
+
+////////////////////////////////////////////////////////////
+const savedUsername = localStorage.getItem('username');
+
+// loginForm에 submit이벤트 발생
+const loginSubmit = function (e) {
+    e.preventDefault();
+
+    // 로그인 정보 저장
+    const username = loginInput.value;
+
+    // 유저 존재 여부 확인
+    if (username === null) return;
+    else localStorage.setItem('username', username);
+
+    loginWelcome(username);
+};
+
+// 초기화
+const init = function () {
+    // prettier-ignore
+    [welcome, workouts, workoutAdd, workoutForm, logoutBtn, imgBox, dotBox].forEach((el) => {el.classList.add('hidden');});
+
+    // 로컬 저장소 비우기
+    window.localStorage.removeItem('username');
+
+    // 로그인 화면 초기화
+    loginForm.classList.remove('hidden');
+    loginForm.reset();
+
+    // 로그인 대기
+    loginForm.addEventListener('submit', loginSubmit);
+    if (savedUsername !== null) loginWelcome(savedUsername);
+};
+
+const showWorkoutForm = function () {
+    workoutForm.classList.remove('hidden');
+};
+
+// 로그인 성공 시
+function loginWelcome(username) {
+    welcome.innerText = `${username}님의 운동 일기`;
+
+    loginForm.classList.add('hidden');
+    workoutForm.classList.add('hidden');
+    [welcome, workouts, workoutAdd, logoutBtn, imgBox, dotBox].forEach((el) => {
+        el.classList.remove('hidden');
+    });
+
+    // 운동 추가 버튼 구현
+    workoutAdd.addEventListener('click', showWorkoutForm);
+    logoutBtn.addEventListener('click', init);
+}
+
+init();
--- js/workoutl_list.js
+++ js/workoutl_list.js
@@ -6,3 +6,51 @@
 // STEP 4 : workout_form input 값 배열로 저장 => workout 내용으로 추가
 // STEP 4 : workout_form => insertAdjacentHTML('afterend', 추가할 내용)
 // STEP 5 : workout_form => hidden class add
+
+const workoutFormInput = function (e) {
+    e.preventDefault();
+
+    const workoutType = document.querySelector('.workout_form_input-type').value;
+    const machine = document.querySelector('.workout_form_input-machine').value;
+    const weight = document.querySelector('.workout_form_input-weight').value;
+    const reps = document.querySelector('.workout_form_input-reps').value;
+    const sets = document.querySelector('.workout_form_input-sets').value;
+
+    // workout HTML
+    const workoutHTML = `
+        <li class="workout">
+            <h2 class="workout_title">Today Workout : ${workoutType}</h2>
+            <div class="workout_details">
+                <span class="workout_icon">🏋️</span>
+                <span class="workout_value">${machine}</span>
+            </div>
+            <div class="workout__details">
+                <span class="workout_icon">🔥</span>
+                <span class="workout_value">${weight}</span>
+                <span class="workout_unit">kg</span>
+            </div>
+            <div class="workout__details">
+                <span class="workout_icon">🎯</span>
+                <span class="workout_value">${reps}</span>
+                <span class="workout_unit">회</span>
+            </div>
+            <div class="workout__details">
+                <span class="workout_icon">⚡️</span>
+                <span class="workout_value">${sets}</span>
+                <span class="workout_unit">set</span>
+            </div>
+        </li>
+    `;
+
+    // STEP 5: workout_form 아래에 운동 내용을 추가
+    workoutForm.insertAdjacentHTML('afterend', workoutHTML);
+
+    // STEP 6: workout_form에 hidden 클래스 추가하여 폼 숨기기
+    workoutForm.classList.add('hidden');
+
+    // 입력된 값 초기화
+    workoutForm.reset();
+};
+
+workoutAdd.addEventListener('click', showWorkoutForm);
+workoutForm.addEventListener('submit', workoutFormInput);
